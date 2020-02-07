91e3c252ecdd96631b6e8779774acdecbf1f4342
kevinmikez12 <51221658+kevinmikez12@users.noreply.github.com>
2020-02-06 23:31:34 -0600

    Add files via upload

---
+++ Practica_3/Practica_3/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_3
+{
+    class Cliente
+    {
+        string name;
+        string address;
+
+        public Cliente()
+        {
+        }
+        public Cliente(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getAddress()
+        {
+            return name;
+        }
+    }
+}
---
+++ Practica_3/Practica_3/Pastel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_3
+{
+    class Pastel
+    {
+        double price, cost;
+        string name;
+
+        public Pastel()
+        {
+        }
+        public Pastel(double price, double cost , string name)
+        {
+            this.price = price;
+            this.cost = cost;
+            this.name = name;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+        public double getCost()
+        {
+            return cost;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+        public void setCost(double cost)
+        {
+            this.cost = cost;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+ 
+    }
+}
